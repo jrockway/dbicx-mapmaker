ec559835e7d7f4d74770c95f72a365a79b1f45cf
Jonathan Rockway <jon@jrock.us>
2008-05-17 18:02:06 +0900

    add lots more docs

--- lib/DBICx/MapMaker.pm
+++ lib/DBICx/MapMaker.pm
@@ -125,12 +125,106 @@ DBICx::MapMaker - automatically create a mapping table
 
 =head1 SYNOPSIS
 
-A common SQL pattern is the "many to many" relationship; where a row
-in the "left table" may point to many rows in the "right table", and a
-row in the "right table" may point to many rows in the "left table".
+A common SQL pattern is the "many to many" relationship; a row in the
+"left table" may point to many rows in the "right table", and a row in
+the "right table" may point to many rows in the "left table".  This
+module automatically creates a L<DBIx::Class|DBIx::Class> result
+source for that table, and sets up the six necessary relationships.
 
-This module automatically creates a L<DBIx::Class|DBIx::Class> result
-source for that table, and sets up all the necessary relationships.
+Here's how to use it.  Imagine you have a table called C<MySchema::A>
+and C<MySchema::B>, each with a primary key.  To create the mapping
+table, you'll write a module like this:
 
-Here's how to use it.  Imagine you have a table called C<A> and C<B>,
-each with a primary key.  To create the mapping table:
+  package MySchema::MapAB;
+  use DBICx::MapMaker;
+  use base 'DBIx::Class';
+  
+  my $map = DBICx::MapMaker->new(
+      left_class  => 'MySchema::A',
+      right_class => 'MySchema::B',
+      left_name   => 'a',
+      right_name  => 'b',
+  );
+    
+  $map->setup_table(__PACKAGE__);
+
+Then, you can:
+
+  my $a = $schema->resultset('A')->find(1);
+  $a->b_map; # the mapping table
+  $a->bs;    # a list of bs that this a has
+
+  my $b = $schema->resultset('B')->find(42);
+  $b->a_map; # the mapping table
+  $b->as;    # a list of as that this b has
+
+=head1 METHODS
+
+=head2 new
+
+Create a C<MapMaker>.  See L</ATTRIBUTES> below for a description of
+the attributes you can pass to the constructor.
+
+=head2 setup_table($class)
+
+Makes C<$class> into the mapping table.  C<$class> should be a
+subclass of C<DBIx::Class>.
+
+=head1 ATTRIBUTES
+
+Here are the attributes that you can pass to the constructor:
+
+=head2 left_class right_class
+
+The class name of the left/right table (the tables that have a m2m
+relationship between them).
+
+Required.
+
+=head2 left_name right_name
+
+The column name for the left/right table's primary key in the map
+table.
+
+Required.
+
+=head2 left_to_map_relation right_to_map_relation
+
+The name of the relationship from the left/right table to the map
+table.
+
+Optional.  Defaults to the name of the opposite table's name with
+C<_map> appended.  (If C<right_name> is C<foo>, then
+C<left_to_map_relation> will be C<foo_map>.)
+
+=head2 rights_from_left lefts_from_right
+
+The name of the m2m relationship.  C<rights_from_left> is the method
+you'll call on a C<left> row to get the corresponding C<right>s.
+(C<lefts_from_right> is the opposite.)
+
+Optional.  Defaults to the name of the row returned with "s" appended.
+If C<left_name> is "foo", then C<lefts_from_right> will be "foos" by
+default.
+
+=head2 tablename
+
+The name of the created mapping table.
+
+Optional.  Defaults to "map_C<left_name>_C<right_name>".  (With C<foo>
+and C<bar>, C<map_foo_bar>.)
+
+=head1 AUTHOR
+
+Jonathan Rockway C<< <jrockway@cpan.org> >>
+
+Stevan Little C<< <stevan.little@iinteractive.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2008 Infinity Interactive, Inc.
+
+L<http://www.iinteractive.com>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
